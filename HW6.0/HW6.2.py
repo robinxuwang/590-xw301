67340124af1c07eabc6e758ab7f67abe44788bcb
robinxuwang <xzw0056@gmail.com>
2021-11-16 12:23:31 -0500

    HW6

---
+++ HW6.0/HW6.2.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 22:48:38 2021
+
+@author: XuWan
+"""
+
+#CNN = True
+# CNN = False
+# DFF_ANN = True
+#DFF_ANN = False
+folds = 5 
+NKEEP=60000
+batch_size=int(0.1*NKEEP)
+epochs=10
+
+#MODEL
+n_bottleneck=50
+
+from keras.datasets import mnist
+from keras.datasets import fashion_mnist
+import numpy as np
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+#from keras.utils import to_categorical
+
+from keras.callbacks import CSVLogger
+csv_logger = CSVLogger('6.2_log.txt', append=True, separator=';')
+
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(train_fashion_img, train_fashion_labels), (test_fashion_img, test_fashion_labels) = fashion_mnist.load_data()
+
+train_images = train_images/np.max(train_images)
+train_fashion_img = train_fashion_img/np.max(train_fashion_img)
+
+train_images = train_images.reshape((60000, 28*28))
+test_images = test_images.reshape((10000, 28*28))
+
+train_fashion_img = train_fashion_img.reshape((60000, 28*28))
+test_fashion_img = test_fashion_img.reshape((10000, 28*28))
+
+
+###############################################################################
+                              #Convolutional AE#
+###############################################################################
+model = models.Sequential()
+
+# #ENCODER
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+model.add(layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+model.add(layers.Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+
+
+model.add(layers.Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(layers.UpSampling2D((2, 2)))
+model.add(layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(layers.UpSampling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.UpSampling2D((2, 2)))
+model.add(layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same'))
+
+
+history = model.fit(train_images, train_images, epochs=epochs, batch_size=1000,validation_split=0.2,callbacks = [csv_logger])       
+model.compile(optimizer='rmsprop',
+                loss='mean_squared_error')
+model.summary()
+
+history_dict = history.history
+
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+results = model.evaluate(train_images, train_images)
+print('The loss of train MNIST is:', results)
+results_f = model.evaluate(train_fashion_img, train_fashion_img)
+print('The loss of train MNIST_Fashion is:', results_f)
+
+plt.plot(loss_values, 'r', label='Training loss')
+plt.plot(val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss of MNIST')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('6.2_loss_hist.png')
+plt.show()
+
+plt.clf()
+
+#anomalies detection for the fashion MINST DATASET
+threshold = 4 * model.evaluate(train_images,train_images)
+predict_result = model.predict(train_fashion_img)
+
+#loop through the data to find any data points that is beyond the threshold
+count = 0  
+for i in range(train_fashion_img.shape[0]):
+    if np.mean((train_fashion_img[i] - predict_result[i])**2) > threshold:
+        count += 1
+        
+print('The number of anomaly in the data is: ',count )
+
+
+
+
